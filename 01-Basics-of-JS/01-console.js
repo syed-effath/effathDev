df130836d1a8b966ce275f67198cfc4fe9083145
syed-effath <rafiyaeffath@gmail.com>
2025-07-16 05:49:49 +0000

    dayOne

---
+++ 01-Basics-of-JS/01-console.js
@@ -0,0 +1,10 @@
+let name = 'syed rafiya effath';
+let age = 21;
+let gender = 'female';
+  //  index =      0           1
+let hobbies = ['coding' , 'eating'];
+
+console.log(`Name: ${name} `);
+console.log(`Age: ${age} \nGender: ${gender}`);
+console.log(`Hobbies: ${hobbies}`);
+console.log(`Hobbies: ${hobbies[0]}`);
